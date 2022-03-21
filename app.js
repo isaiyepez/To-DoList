6aa9503ce93b4bee0b9d90442c9ca0b1a9528073
Isai Yepez <isaiyepez@gmail.com>
2022-03-20 20:27:05 -0600

    Final version

--- app.js
+++ app.js
@@ -1,39 +1,56 @@
 //jshint esversion:6
 
 const express = require("express");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-var tasks = [];
+
+// Seems crazy, but you can define this as constant.
+// in JS, you can push new items to the array,
+// and also can change the value of an item, but
+// you can't assign a new array value (e.g. tasks = workItems will not work)
+const tasks = [];
+const workItems = [];
 
 //You must use this line of code below express variable def.
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+//To make express to serve up the public folder:
+app.use(express.static("public"));
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
 
 app.get("/", function(req, res) {
-
-
-
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
     
-    res.render("list", {kindOfDay: day, listItems: tasks})
+    let longDate = date.getDate();    
+    
+    res.render("list", {listTitle: longDate, listItems: tasks})
 });
 
 app.post("/", function(req, res) {
-    newTask = req.body.task;
-    console.log(newTask);
+    
+    let newTask = req.body.task;
 
-    tasks.push(newTask);
+    if (req.body.list === "Work") {
+        workItems.push(newTask);
+        res.redirect("/work");
+    } else {
 
-    res.redirect("/");
+        tasks.push(newTask);
+        res.redirect("/");
+    }
+
 });
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", listItems: workItems})
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
